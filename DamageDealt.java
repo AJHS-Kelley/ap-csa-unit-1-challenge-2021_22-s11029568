b856c07940bce4d5a849f43f298def28a6da271c
Venti <70521040+s11029568@users.noreply.github.com>
2021-10-20 09:00:42 -0400

    v0.5
    imported a scanner and allowed the player to input a weapon

---
+++ DamageDealt.java
@@ -0,0 +1,16 @@
+// Maxximus Secrest, AP CSA Unit 1 Challenge, Andrew Jackson High School, <10/20/2021>, <9:00>, v0.5
+import java.util.Scanner;
+
+public class DamageDealt {
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+        
+        System.out.println("Please Enter the name of the weapon:");
+
+        String weaponName = myScanner.nextLine();
+
+
+    
+    }
+
+}
