733993a0b1370f753861c5117ba6b3618f1e9154
Venti <70521040+s11029568@users.noreply.github.com>
2021-10-20 09:17:55 -0400

    v1.0
    Asks the player for the damage of the weapon

--- DamageDealt.java
+++ DamageDealt.java
@@ -1,16 +1,14 @@
-// Maxximus Secrest, AP CSA Unit 1 Challenge, Andrew Jackson High School, <10/20/2021>, <9:00>, v0.5
+// Maxximus Secrest, AP CSA Unit 1 Challenge, Andrew Jackson High School, <10/20/2021>, <9:17>, v1.0
 import java.util.Scanner;
 
 public class DamageDealt {
     public static void main(String[] args) {
         Scanner myScanner = new Scanner(System.in);
-        
         System.out.println("Please Enter the name of the weapon:");
-
         String weaponName = myScanner.nextLine();
-
-
-    
+        System.out.println("   ");
+        System.out.println("Please enter the damage value of the weapon");
+        double weaponDamage = myScanner.nextDouble();
     }
 
 }
