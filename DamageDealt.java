e254724a665b82bed0dd882a40d1406826a8f36c
Venti <70521040+s11029568@users.noreply.github.com>
2021-10-22 09:30:13 -0400

    2.0 again
    same thing but fixed an issue with the monstername

--- DamageDealt.java
+++ DamageDealt.java
@@ -1,19 +1,25 @@
-// Maxximus Secrest, AP CSA Unit 1 Challenge, Andrew Jackson High School, <10/20/2021>, <9:19>, v2,0
+// Maxximus Secrest, AP CSA Unit 1 Challenge, Andrew Jackson High School, <10/22/2021>, <9:29>, v2.0 again
 // pear debugging by: robin/rachel macdonald (lol hi maxx)
 import java.util.Scanner;
 
 public class DamageDealt {
     public static void main(String[] args) {
         Scanner myScanner = new Scanner(System.in);
+        System.out.println("MonsterName");
+        String monsterName = myScanner.nextLine();
+        System.out.print("\n");
         System.out.println("Please Enter the name of the weapon:");
         String weaponName = myScanner.nextLine();
         System.out.println("   ");
         System.out.println("Please enter the damage value of the weapon");
         double weaponDamage = myScanner.nextDouble();
-        System.out.println("   ");
-        System.out.println("What is the name of the monster?");
-        String monsterName = myScanner.nextLine();
-        System.out.println(monsterName);
+        System.out.println("\n");
+        System.out.println("How many attacks will you perform?");
+        int attacknumber = myScanner.nextInt();
+    
+        
+       
+        
     }
 
 }
